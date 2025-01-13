fde76e6c872141deafea3258993993ba726443fb
tea <mgringo9@gmail.com>
2025-01-13 22:07:54 +0530

    CORS error Landing DataRow controller debug 564436334dd3deeeee apio fnl 8

--- src/pages/my-product-detail-edit-page.tsx
+++ src/pages/my-product-detail-edit-page.tsx
@@ -240,6 +240,7 @@ const MyProductDetailEditPage = ({ product }: UseFormProps) => {
         method: "POST",
         headers: {
           Accept: "application/json",
+          "Content-Type": "multipart/form-data",
           "Access-Control-Allow-Origin": "*", // Required for CORS support to work
           Authorization: "Bearer " + localStorage.getItem("token"),
         },
