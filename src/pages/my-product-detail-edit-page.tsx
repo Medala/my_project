aae0bcc3472083c1ff7189b06cd366d9f61245eb
tea <mgringo9@gmail.com>
2025-01-13 20:04:28 +0530

    CORS error Landing DataRow controller debug 564436334dd3d

--- src/pages/my-product-detail-edit-page.tsx
+++ src/pages/my-product-detail-edit-page.tsx
@@ -238,7 +238,7 @@ const MyProductDetailEditPage = ({ product }: UseFormProps) => {
       const response = await fetch(updateProductUrl + "/" + productId, {
         method: "PATCH",
         headers: {
-          "Content-Type": "multipart/form-data",
+          "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*", // Required for CORS support to work
           Authorization: "Bearer " + localStorage.getItem("token"),
         },
